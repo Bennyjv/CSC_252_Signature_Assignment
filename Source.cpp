895fd502ef5cbbb982c980541cc65e431bca3fca
benny valdez <Bennyv-1@hotmail.com>
2026-04-14 15:02:19 -0700

    hi

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,2 @@
-// Hi Team
+// Hi Team (Benny, Navada, Eddie) this is my source code for the Signature assignment. I'll be adding it to github to collaborate and share my code with you.
+// I'm still learning the in and outs of git hub. I did make braches for each of us to work on our own code if you want to and Nevada can takes parts of our code.
